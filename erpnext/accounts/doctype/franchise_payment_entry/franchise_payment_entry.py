0b143750c9d5d7767cb7d76d4b3ea57e54f12ba7
SherinKR <sherinkrply@gmail.com>
2021-07-29 14:43:21 +0530

    Script updated from core

--- erpnext/accounts/doctype/franchise_payment_entry/franchise_payment_entry.py
+++ erpnext/accounts/doctype/franchise_payment_entry/franchise_payment_entry.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2021, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.mapper import get_mapped_doc
 
 class FranchisePaymentEntry(Document):
 	pass
+
+@frappe.whitelist()
+def make_payment_entry(source_name, target_doc=None):
+	def set_missing_values(source, target):
+		target.naming_series = "ACC-PAY-.YYYY.-"
+		target.payment_type = "Pay"
+		target.party_type =  "Supplier"
+		target.party = "Internal Supplier SG"
+		target.party_name = "Internal Supplier SG"
+		target.paid_amount = source.total_purchase_amount
+		target.received_amount = source.total_purchase_amount
+		target.franchise_payment_entry = source.name
+
+	doclist = get_mapped_doc("Franchise Payment Entry", source_name,{
+		"Franchise Payment Entry": {
+			"doctype": "Payment Entry",
+		},
+	}, target_doc, set_missing_values)
+
+	return doclist

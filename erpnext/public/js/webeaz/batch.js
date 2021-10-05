c6bbaaeb3ded7873a9f8d64aaa89db4f90d7bca9
SherinKR <sherinkrply@gmail.com>
2021-10-05 16:39:23 +0530

    fix:Batch Dashbord restriction for franchise

--- erpnext/public/js/webeaz/batch.js
+++ erpnext/public/js/webeaz/batch.js
@@ -16,5 +16,23 @@ frappe.ui.form.on('Batch', {
 		    frm.set_value('batch_id', frm.doc.batch_name+"-"+frm.doc.item);
 		    frm.refresh_field('batch_id');
 		}
+	},
+	onload(frm){
+		hide_dashbord(frm);
+	},
+	refresh(frm){
+		hide_dashbord(frm);
 	}
 });
+
+function hide_dashbord(frm){
+	if(frappe.defaults.get_user_default("Company")){
+		var company=frappe.defaults.get_user_default("Company")
+		frappe.db.get_value('Company', company, 'is_group', function(r) {
+			if(r && r.is_group != 1){
+				console.log("Franchise");
+				frm.dashboard.hide()
+			}
+		});
+	}
+}

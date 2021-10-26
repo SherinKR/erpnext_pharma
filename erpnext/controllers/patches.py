f7d4facd5b987cb008d7118949a3c509727e6692
SherinKR <sherinkrply@gmail.com>
2021-10-26 10:01:36 +0530

    fix: Button to update missing price on company

--- erpnext/controllers/patches.py
+++ erpnext/controllers/patches.py
@@ -7,21 +7,29 @@ from frappe import publish_progress
 def create_item_price_without_batch(price_list):
     if price_list=="Price To Franchaisee - (PTF)":
         item_price_list = frappe.get_list("Item Price", filters={'price_list': price_list})
+        count = 0
+        # frappe.msgprint('Item Price updation started')
         for item_price in item_price_list:
+            frappe.publish_progress(count*100/len(item_price_list), title = ("Creating Dimensions..."))
             item_price_doc = frappe.get_doc("Item Price", item_price.name)
             if not frappe.db.exists({"doctype":"Item Price", 'item_code':item_price_doc.item_code ,'price_list': price_list, 'batch_no':""}):
-                print("in if")
                 new_item_price_doc = frappe.new_doc('Item Price')
                 new_item_price_doc.price_list = price_list
                 new_item_price_doc.item_code = item_price_doc.item_code
                 new_item_price_doc.price_list_rate = item_price_doc.price_list_rate
                 new_item_price_doc.insert()
                 frappe.db.commit()
+            count = count+1
         frappe.msgprint("Item Price Updation Completed")
 
 @frappe.whitelist()
 def update_item_price_from_button(price_list):
+    frappe.msgprint("Enqueeddd")
     # publish_progress(percent=10, title="Reading the file")
     # frappe.show_progress('Loading..', 70, 100, 'Please wait')
     # frappe.publish_realtime("ocr_progress_bar", {"progress": [5, 10], "reload": 1}, user=frappe.session.user)
-    frappe.enqueue(create_item_price_without_batch,price_list=price_list, is_async=True, queue="long")
+    # frappe.enqueue(create_item_price_without_batch,price_list=price_list, is_async=True, queue="short")
+    # create_item_price_without_batch(price_list)
+    # for i in range(1,100):
+    #     print(i)
+    #     frappe.publish_progress(i, title="Updating Variants...")
--- erpnext/public/js/webeaz/company.js
+++ erpnext/public/js/webeaz/company.js
@@ -1,51 +1,52 @@
 frappe.ui.form.on('Company', {
 	refresh: function(frm) {
-    frm.add_custom_button(__('Set Item Defaults'), function() {
-		frappe.msgprint(__('Item defaults updation started'));
-  		frappe.call({
-  			"method": "erpnext.controllers.item_setup.set_item_defaults",
-  			"args": {
-  				"company": frm.doc.company_name,
-				"warehouse": frm.doc.default_warehouse,
-				"price_list": frm.doc.default_price_list,
-				"supplier": frm.doc.default_supplier
-  			},
-  			callback: function(r){
-  				console.log(r);
-					frappe.msgprint(__('Item defaults succesfully updated'));
-  			}
-  		});
-  	});
+		frm.add_custom_button(__('Set Item Defaults'), function() {
+			frappe.msgprint(__('Item defaults updation started'));
+			frappe.call({
+				"method": "erpnext.controllers.item_setup.set_item_defaults",
+				"args": {
+					"company": frm.doc.company_name,
+					"warehouse": frm.doc.default_warehouse,
+					"price_list": frm.doc.default_price_list,
+					"supplier": frm.doc.default_supplier
+				},
+				callback: function(r){
+					console.log(r);
+						frappe.msgprint(__('Item defaults succesfully updated'));
+				}
+			});
+		});
 
-	frm.add_custom_button(__('Set Tax Defaults'), function() {
-		frappe.msgprint(__('Tax defaults updation started'));
-  		frappe.call({
-  			"method": "erpnext.controllers.item_setup.set_tax_defaults",
-  			"args": {
-  				"company": frm.doc.company_name,
-				"warehouse": frm.doc.default_warehouse,
-				"price_list": frm.doc.default_price_list,
-				"tax_template": frm.doc.default_tax_template,
-				"supplier": frm.doc.default_supplier
-  			},
-  			callback: function(r){
-  				console.log(r);
-					frappe.msgprint(__('Tax defaults succesfully updated'));
-  			}
-  		});
-  	});
-
-	frm.add_custom_button(__('Set Missing Item Price'), function() {
-		frappe.msgprint(__('Item Price updation started'));
-  		frappe.call({
-  			"method": "erpnext.controllers.patches.update_item_price_from_button",
-  			"args": {
-  				"price_list": "Price To Franchaisee - (PTF)"
-  			},
-  			callback: function(r){
-  				console.log(r);
-  			}
-  		});
-  	});
-  }
+		frm.add_custom_button(__('Set Tax Defaults'), function() {
+			frappe.msgprint(__('Tax defaults updation started'));
+			frappe.call({
+				"method": "erpnext.controllers.item_setup.set_tax_defaults",
+				"args": {
+					"company": frm.doc.company_name,
+					"warehouse": frm.doc.default_warehouse,
+					"price_list": frm.doc.default_price_list,
+					"tax_template": frm.doc.default_tax_template,
+					"supplier": frm.doc.default_supplier
+				},
+				callback: function(r){
+					console.log(r);
+						frappe.msgprint(__('Tax defaults succesfully updated'));
+				}
+			});
+		});
+		if(frm.doc.is_group){
+			frm.add_custom_button(__('Set Missing Item Price'), function() {
+				frappe.msgprint(__('Item Price updation started'));
+				frappe.call({
+					"method": "erpnext.controllers.patches.create_item_price_without_batch",
+					"args": {
+						"price_list": "Price To Franchaisee - (PTF)"
+					},
+					callback: function(r){
+						console.log(r);
+					}
+				});
+			});
+		}
+	}
 });
